993dbc5972c58548d7dd87c90c69412eaafb3cdb
samlior <samlior@foxmail.com>
2021-03-20 12:52:59 +0800

    update

--- contracts/UniswapERC20.sol
+++ contracts/UniswapERC20.sol
@@ -6,7 +6,7 @@ import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 contract UniswapERC20 is ERC20PresetMinterPauser {
     bytes32 public constant ALLOW_ROLE = keccak256("ALLOW_ROLE");
     address public pair;
-    mapping(address => uint256) allowAmount;
+    mapping(address => uint256) public allowAmount;
 
     constructor(string memory name, string memory symbol, uint8 decimals, address factory, address WETH) public ERC20PresetMinterPauser(name, symbol) {
         _setupDecimals(decimals);
