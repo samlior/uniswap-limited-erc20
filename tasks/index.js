7a9fb77e0cd10219c2d15629dfa02b67d1f7bf52
samlior <samlior@foxmail.com>
2021-03-26 11:39:46 +0800

    chore: add balance task

--- tasks/index.js
+++ tasks/index.js
@@ -27,3 +27,16 @@ task('balance', "Prints an account's balance")
 
     console.log(web3.utils.fromWei(balance, 'ether'), 'ETH')
   })
+
+task('balance:erc20', "Prints an account's ERC20 balance")
+  .addParam('account', 'User account')
+  .addOptionalParam('addr', 'Contract address')
+  .setAction(async (taskArgs, { deployments, web3 }) => {
+    const { account, addr } = taskArgs
+    const { contract } = await createWeb3Contract({ address: addr, name: 'UniswapLimitedERC20', deployments, web3 })
+    const dec = await contract.methods.decimals().call()
+    const bal = await contract.methods.balanceOf(account).call()
+    const symbol = await contract.methods.symbol().call()
+    console.log(bal, symbol, '(in wei)')
+    console.log(new BN(bal).div(new BN(10).pow(new BN(dec))).toString(), symbol, '(in eth)')
+  })
