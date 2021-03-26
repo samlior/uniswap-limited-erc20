f858ef965136f0a5d39b0738e19d8a173915779b
samlior <samlior@foxmail.com>
2021-03-26 11:23:05 +0800

    chore: update

--- deploy/deploySettings.js
+++ deploy/deploySettings.js
@@ -6,3 +6,5 @@ module.exports = {
   weth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
   codeHash: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
 }
+
+module.exports.skip = () => Promise.resolve(true)
